8bdea95a48c8abc1f5e214bb2689ea8ca264a177
pixel4-ui <pixel4a7610@gmail.com>
2026-09-25 23:36:43 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static('public'));
+app.get('*', (req,res)=> res.sendFile(path.join(__dirname,'public','index.html')));
+app.listen(process.env.PORT || 10000, ()=> console.log('Live'));
